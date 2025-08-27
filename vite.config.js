82c7471483e58536ad150392a8031c3e53e339b4
vijoy-paul <vijoypaul2000@gmail.com>
2025-08-27 13:07:30 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/.netlify/functions'),
+          secure: true,
+        },
+      },
+    },
+  });
+};
